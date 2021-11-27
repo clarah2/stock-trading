b101fb39a589d6b780fedd8966ea84b4323e7f30
mohsaad <msaad96@gmail.com>
2021-11-27 01:10:40 -0500

    finished multiple, need to debug

--- stock_env_multiple.py
+++ stock_env_multiple.py
@@ -3,21 +3,26 @@
 import numpy as np
 import csv
 import pandas as pd
-import talib
 
-INITIAL_AMOUNT = 1000
+INITIAL_AMOUNT = 1e6
 
-class StockEnv:
-    def __init__(self, tickers=None, begin_date=None, end_date=None, initial_amount=INITIAL_AMOUNT, initial_stocks=[0]):
-        self.df = pd.read_csv('data/coke.csv', header=0)
+class StockEnvMultiple:
+    def __init__(self, tickers=None, begin_date=None, end_date=None, initial_amount=INITIAL_AMOUNT, initial_stocks=None):
+        df_raw = pd.read_csv('data/filtered_with_ti.csv', header=0)
         self.spy = pd.read_csv('data/SPY.csv', header=0)
-        self.start_day = 35
+        
+        # Filter dataframe by stocks we want.
+        self.df = df_raw[df_raw['tic'].isin(tickets)]
+        
+        # Initialize datetime range
+        self._initialize_datetime_range()
+        
+        self.start_day = 0
         self.day = self.start_day
         self.open_price, self.close_price, self.rsi, self.macd, self.obv, self.cci, self.adx = self._get_data(self.day)
 
-        self.max_step = self._get_length_of_data()
-        
-        self.max_day = 1000
+        self.max_step = self.daterange.shape[0]
+        self.max_day = self.get_index(end_date) if self.get_index(end_date) is not None else 10000
         
         self.initial_amount = initial_amount
         # Amount of money we have.
@@ -44,10 +49,60 @@ class StockEnv:
         # Stuff needed for identification
         self.env_name = "RLStockEnv-v1"
         self.state_dim = len(self.reset())
-        self.action_dim = 1
-        self.target_return = 12345.0
+        self.action_dim = len(self.stocks)
+        self.target_return = 1e7
         self.if_discrete = False
 
+        
+    """
+    Get the datetime range (from AAPL since we know it has the full datetime)
+    """
+    def _initialize_datetime_range(self):
+        aapl = self.df[self.df['tic'] == 'AAPL']
+        self.daterange = aapl['Date'].to_numpy()
+    
+    """
+    Convert date to time index.
+    """
+    def get_index(self, date):
+        # First convert date to pandas date.
+        pd_date = pd.to_datetime(date, infer_datetime_format=True)
+        
+        # get index in numpy array
+        out = np.where(self.daterange == pd_date)
+        
+        # Return nothing if no index found.
+        if len(out) == 0:
+            return None
+        
+        return out[0]
+    
+    """
+    Convert time index to date.
+    """
+    def get_date(self, index):
+        if index >= self.daterange.shape[0]:
+            return None
+        return self.daterange[index]
+    
+    """
+    Get data and technical indicators given a time index.
+    """
+    def _get_data(self, index):
+        # Get state variables
+        stock_idxs = self.df['Date'] == self.get_date(index)
+        tics = self.df[stock_idxs]['tic'].tolist()
+        open_price = self.df[stock_idxs]['Open'].tolist()
+        close_price = self.df[stock_idxs]['Close'].tolist()
+        rsi = self.df[stock_idxs]['rsi'].tolist()
+        macd = self.df[stock_idxs]['macd'].tolist()
+        obv = self.df[stock_idxs]['obv'].tolist()
+        cci = self.df[stock_idxs]['cci'].tolist()
+        adx = self.df[stock_idxs]['adx'].tolist()
+        
+        return [tic, open_price, close_price, rsi, macd, obv, cci, adx]
+        
+    
     """
     Resets the environment to default.
     """
@@ -56,13 +111,10 @@ class StockEnv:
         self.balance = self.initial_amount
         self.rewards = []
         self.stocks = self.initial_stocks
-        self.open_price, self.close_price, self.rsi, self.macd, self.obv, self.cci, self.adx = self._get_data(self.day)
-        self.total_assets = sum([x*y for x,y in zip([self.close_price], self.stocks)])
-        self.technical_indicators = [self.rsi, self.macd, self.obv, self.cci, self.adx]
-        output = [self.balance, self.stocks[0], self.open_price, self.close_price]
-        for item in self.technical_indicators:
-            output.append(item)
-
+        
+        _, _, close_price, rsi, macd, obv, cci, adx = self._get_data(self.day)
+        self.total_assets = sum([x*y for x,y in zip(close_price, self.stocks)])
+        output = [self.balance, *self.stocks, *close_price, *rsi, *macd, *obv, *cci, *adx]
         return output
 
 
@@ -71,113 +123,39 @@ class StockEnv:
     """
     def step(self, action):
         self.day += 1
-        self.open_price, self.close_price, self.rsi, self.macd, self.obv, self.cci, self.adx = self._get_data(self.day)
-        self.technical_indicators = [self.rsi, self.macd, self.obv, self.cci, self.adx]
+        tic, open_price, close_price, rsi, macd, obv, cci, adx = self._get_data(self.day)
+        
+        action = action * self.max_stock  # actions initially is scaled between 0 to 1
+        action = (action.astype(int))  # convert into integer because we can't by fraction of shares
+        
         done = False
         if self.day == self.max_day:
             done = True
         
         for i in range(self.action_dim):
             stock_action = action[i]
-            close_price = self.close_price
+            close_price_idx = close_price[i]
             if stock_action > 0:
-                #amount of stocks that can be bought with current balance
-                available = self.balance // close_price
+                # amount of stocks that can be bought with current balance
+                available = self.balance // close_price_idx
                 change_in_stock = min(available, stock_action)
-                self.balance -= close_price * change_in_stock
+                self.balance -= close_price_idx * change_in_stock
                 self.stocks[i] += change_in_stock
             elif stock_action < 0:
-                #amount of stocks that can be sold
+                # amount of stocks that can be sold
                 change_in_stock = min(self.stocks[i], -stock_action)
                 self.balance += close_price * change_in_stock
                 self.stocks[i] -= change_in_stock
+      
         # compute value of total assets and add difference from yesterday's to rewards
-        stocks_value = sum([x*y for x,y in zip([self.close_price], self.stocks)])
+        stocks_value = sum([x*y for x,y in zip(close_price, self.stocks)])
         daily_total_assets = stocks_value + self.balance
         daily_reward = daily_total_assets - self.total_assets
         self.rewards.append(daily_reward)
 
-        state = [self.balance, self.stocks[0], self.open_price, self.close_price]
-        for item in self.technical_indicators:
-            state.append(item)
+        state = [self.balance, *self.stocks, *close_price, *rsi, *macd, *obv, *cci, *adx]
         return state, daily_reward, done, dict()
 
-    """
-    Function for buying a stock. Part of our action space.
-    """
-    def _buy_stock(self, stock_index, action):
-        close_price = self.state[3]
-        if close_price > 0:
-            available = self.balance // close_price
-            num_shares_bought = min(available, action)
-            amount_bought = close_price * num_shares_bought
-            self.balance -= amount_bought
-            self.stocks[stock_index] += num_shares_bought
-        else:
-            num_shares_bought = 0
-        return num_shares_bought
-
-    """
-    Function to sell a stock. Part of our action space.
-    """
-    def _sell_stock(self, stock_index, action):
-        close_price = self.state[3]
-        if close_price > 0:
-            #check that agent has some of that stock
-            if self.stocks[stock_index] > 0:
-                num_shares_sold = min(self.stocks[stock_index], abs(action))
-                amount_sold = close_price * num_shares_sold
-                self.balance += amount_sold
-                self.stocks[stock_index] -= num_shares_sold
-            else:
-                num_shares_sold = 0
-        else:
-            num_shares_sold = 0
-        return num_shares_sold
-
-    """
-    Function to compute and add technical indicators to dataframe.
-    """
-    def _compute_technical_indicators(self, df):
-        df['rsi'] = talib.RSI(df['prcod'])
-        df['macd'], df['macdsignal'], df['macdhist'] = talib.MACD(df['prccd']) # defaults: fastperiod=12, slowperiod=26, signalperiod=9
-        df['obv'] = talib.OBV(df['prccd'], df['cshtrd'])
-        df['cci'] = talib.CCI(df['prchd'], df['prcld'], df['prccd']) #default period: 14 days
-        df['adx'] = talib.ADX(df['prchd'], df['prcld'], df['prccd']) #default period: 14 days
-
-        
-    """
-    Converts a time index between 0 and X to a datetime.
-    """
-    def _convert_time_index_to_date_time(self, index):
-         pass
-        
-    """
-    Function to get the open price, close price, and technical indicators for given day.
-    """
-    def _get_data(self, today=35):
-        self._compute_technical_indicators(self.df)
-
-        opening = self.df['prcod']
-        closing = self.df['prccd']
-        rsi, macd, obv, cci, adx = self.df['rsi'], self.df['macd'], self.df['obv'], self.df['cci'], self.df['adx']
-        
-        out = []
-        today_open = opening[today]
-        today_close = closing[today]
-        today_rsi = rsi[today]
-        today_macd = macd[today]
-        today_obv = obv[today]
-        today_cci = cci[today]
-        today_adx = adx[today]
-
-        return today_open, today_close, today_rsi, today_macd, today_obv, today_cci, today_adx
-   
-    def _get_length_of_data(self):
-        return self.df['prccd'].shape[0] - self.start_day
-    
-    def _get_all_days(self):
-        return self.df['prccd'].shape[0]
 
     def draw_cumulative_return(self, args, _torch) -> list:
         state_dim = self.state_dim
